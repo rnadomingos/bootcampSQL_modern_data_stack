6d165b9a8e373d04c593a647a4b84d335854da3e
Renan Domingos <rna.domingos@hotmail.com>
2025-06-21 09:40:05 -0300

    feat: add myfirst DAG

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from time import sleep
+from airflow.decorators import dag, task
+from datetime import datetime
 
-def primeira_atividade():
-    print("primeira atividade rodou com sucesso")
-    sleep(2)
-
-def segunda_atividade():
-    print("segunda atividade rodou com sucesso")
-    sleep(2)
-
-def terceira_atividade():
-    print("terceira atividade rodou com sucesso")
-    sleep(2)
-
+@dag(
+        dag_id="My first DAG",
+        description="First DAG with air flow",
+        schedule="40 9 * * *",
+        start_date=datetime(2024,6,21),
+        catchup=False #backfill
+)
 def pipeline():
-    primeira_atividade()
-    segunda_atividade()
-    terceira_atividade()
+
+    @task
+    def primeira_atividade():
+        print("primeira atividade rodou com sucesso")
+        sleep(2)
+    
+    @task
+    def segunda_atividade():
+        print("segunda atividade rodou com sucesso")
+        sleep(2)
+    
+    @task
+    def terceira_atividade():
+        print("terceira atividade rodou com sucesso")
+        sleep(2)
+
+    t1 = primeira_atividade()
+    t2 = segunda_atividade()
+    t3 = terceira_atividade()
 
 pipeline()    
